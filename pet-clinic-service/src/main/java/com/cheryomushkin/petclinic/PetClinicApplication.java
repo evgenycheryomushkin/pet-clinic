4c4efa3bdc85f570824ec5dd4bcc08718a4cfbd0
Evgeny Cheryomushkin <evgeny.cheryomushkin@gmail.com>
2021-11-20 00:20:49 +0700

    Added CORS filter for localhost:4200

--- pet-clinic-service/src/main/java/com/cheryomushkin/petclinic/PetClinicApplication.java
+++ pet-clinic-service/src/main/java/com/cheryomushkin/petclinic/PetClinicApplication.java
@@ -2,6 +2,10 @@ package com.cheryomushkin.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PetClinicApplication {
@@ -10,4 +14,13 @@ public class PetClinicApplication {
         SpringApplication.run(PetClinicApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/petclinic/api/**").allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
 }
