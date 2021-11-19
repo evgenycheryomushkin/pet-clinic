f7fbcf5c5a86bd7e7107d7f3d3631f8635fcef8d
Evgeny Cheryomushkin <evgeny.cheryomushkin@gmail.com>
2021-11-19 23:29:00 +0700

    Added owner dto and test for converter

--- pet-clinic-api/src/main/java/com/cheryomushkin/petclinic/transport/OwnerDto.java
+++ pet-clinic-api/src/main/java/com/cheryomushkin/petclinic/transport/OwnerDto.java
@@ -3,16 +3,26 @@ package com.cheryomushkin.petclinic.transport;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Data @Builder
 public class OwnerDto {
-    @NotNull @NonNull
+    @Nullable
     Long id;
     @NotNull @NotEmpty @NonNull
     String firstName;
     @NotNull @NotEmpty @NonNull
     String lastName;
+    @NotNull @NotEmpty @NonNull
+    String address;
+    @NotNull @NotEmpty @NonNull
+    String city;
+    @NotNull @NotEmpty @NonNull
+    String telephone;
+    @NotNull @NonNull
+    List<PetDto> pets;
 }
--- pet-clinic-api/src/main/java/com/cheryomushkin/petclinic/transport/PetDto.java
+++ pet-clinic-api/src/main/java/com/cheryomushkin/petclinic/transport/PetDto.java
@@ -2,7 +2,13 @@ package com.cheryomushkin.petclinic.transport;
 
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.lang.NonNull;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data @Builder
 public class PetDto {
+    @NotNull @NotEmpty @NonNull
+    String name;
 }
